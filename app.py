0383f35553c7d34c7d25bec3520f4f6105c39899
Chris Petersen <geek@ex-nerd.com>
2012-06-05 21:08:32 -0700

    give it a name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "The future boozetrack.com"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
