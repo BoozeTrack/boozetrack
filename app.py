c5b2241e4eb56e03f6daf2001bc4cf5a0d7a4646
Chris Petersen <geek@ex-nerd.com>
2012-07-10 01:14:42 -0700

    Port configuration for heroku

--- app.py
+++ app.py
@@ -5,7 +5,13 @@
 from booze import app
 
 if __name__ == '__main__':
+    # Import heroku config, or set defaults
+    port = os.environ.get('PORT')
+    if not port:
+        port = 5000
+    # Run the app
     app.run(
         host='0.0.0.0',
+        port=port,
         debug=True
         )
